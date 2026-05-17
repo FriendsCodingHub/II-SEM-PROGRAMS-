8b1d28052b688be0833ba708f722c6e3badf1698
Theerthananda <theerthanandagowda@gmail.com>
2026-05-18 00:07:25 +0530

    Exit added

--- OS ANd UNIX/C2/controllstmt.sh
+++ OS ANd UNIX/C2/controllstmt.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
-echo "Control Statement Demonstration"
+echo "Control Statements Demo"
 
-for i in 1 2 3 4 5; do
-    if [ $i -eq 3 ]; then
+for i in 1 2 3; do
+    if [ $i -eq 2 ]; then
         continue
     fi
-    echo "FOR Loop Value: $i"
+    echo "FOR Loop: $i"
 done
 
-count=1
+n=1
 
-while [ $count -le 3 ]; do
-    echo "WHILE Loop Count: $count"
-    count=$((count+1))
+while [ $n -le 2 ]; do
+    echo "WHILE Loop: $n"
+    n=$((n+1))
 done
 
-echo "1) Add   2) Subtract"
+echo "1) Add  2) Subtract  3) Exit"
 read ch
 
 case $ch in
-1) echo "Addition: $((5+3))" ;;
-2) echo "Subtraction: $((5-3))" ;;
-*) echo "Invalid Choice" ;;
+1) echo "Sum = $((5+2))" ;;
+2) echo "Sub = $((5-2))" ;;
+3) echo "Program Exited"; exit ;;
+*) echo "Wrong Choice" ;;
 esac
