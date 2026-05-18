504aea0c82ac11c320560468a551e0bc594c9bb6
Theerthananda <theerthanandagowda@gmail.com>
2026-05-18 19:51:45 +0530

    Constructor ovreloading

--- NET/C2_Programs/C2_Programs/ConstructorOverloadingDemo.cs
+++ NET/C2_Programs/C2_Programs/ConstructorOverloadingDemo.cs
@@ -6,29 +6,41 @@ namespace ConstructorOverloadingProgram
     {
         int id;
         string name;
+        string course;
 
         public Student()
         {
             id = 0;
             name = "Unknown";
+            course = "Not Assigned";
         }
 
         public Student(int i)
         {
             id = i;
             name = "Not Assigned";
+            course = "Not Assigned";
         }
 
         public Student(int i, string n)
         {
             id = i;
             name = n;
+            course = "Not Assigned";
+        }
+
+        public Student(int i,string n,string c)
+        {
+            id=i;
+            name = n;
+            course = c;
         }
 
         public void Display()
         {
             Console.WriteLine("ID: " + id);
             Console.WriteLine("Name: " + name);
+            Console.WriteLine("Course: "+course);
             Console.WriteLine();
         }
     }
@@ -40,11 +52,11 @@ namespace ConstructorOverloadingProgram
 
             Student s1 = new Student();
 
-  
             Student s2 = new Student(101);
 
-    
             Student s3 = new Student(102, "Rahul");
+
+            Student s4 = new Student(102, "Rahul","Msc");
 
             Console.WriteLine("Default Constructor:");
             s1.Display();
@@ -54,6 +66,9 @@ namespace ConstructorOverloadingProgram
 
             Console.WriteLine("Two Parameter Constructor:");
             s3.Display();
+
+            Console.WriteLine("Three Parameter Constructor:");
+            s4.Display();
         }
     }
 }
